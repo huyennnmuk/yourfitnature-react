9bb068523fa01f62b612166a67aa92af45f683c4
huyennnmuk <huyennnm.uk@gmail.com>
2025-09-26 16:19:47 +0700

    fix: Untrack remaining JSON   file from LFS

--- system/Error/error.js
+++ system/Error/error.js
@@ -1,9 +1,18 @@
- GET /about-us 500 in 37ms
- GET /about-us 500 in 5ms
- ⨯ ./node_modules/framer-motion/dist/es/index.mjs
-Error: It's currently unsupported to use "export *" in a client boundary. Please use named exports instead.
-Import trace for requested module:
-./node_modules/framer-motion/dist/es/index.mjs  
-./app/components/ValueProposition.tsx
-./app/about-us/page.tsx
- ⨯ TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received undefined
+Running build in Washington, D.C., USA (East) – iad1
+Build machine configuration: 2 cores, 8 GB
+Cloning github.com/huyennnmuk/yourfitnature-react (Branch: master, Commit: 2054636)
+Previous build caches not available
+Cloning completed: 22.790s
+Running "vercel build"
+Vercel CLI 48.1.6
+Installing dependencies...
+npm warn deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+npm warn deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
+npm warn deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
+npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+npm warn deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+added 1083 packages in 29s
+275 packages are looking for funding
+  run `npm fund` for details
