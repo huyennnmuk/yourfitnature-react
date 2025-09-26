9f7fb7388c57080a8b61b2607118893567362561
huyennnmuk <huyennnm.uk@gmail.com>
2025-09-26 17:27:59 +0700

    fix: Generate both prisma clients in postinstall

--- system/Error/error.js
+++ system/Error/error.js
@@ -1,10 +1,18 @@
-  ▲ Next.js 14.2.32
-   Creating an optimized production build ...
- ⚠ Found lockfile missing swc dependencies, run next locally to automatically patch
-   Disabled SWC as replacement for Babel because of custom Babel configuration "babel.config.js" https://nextjs.org/docs/messages/swc-disabled
-   Using external babel configuration from /vercel/path0/babel.config.js
-[BABEL] Note: The code generator has deoptimised the styling of /vercel/path0/node_modules/react-icons/fa/index.mjs as it exceeds the max of 500KB.
-[BABEL] Note: The code generator has deoptimised the styling of /vercel/path0/node_modules/react-icons/bi/index.mjs as it exceeds the max of 500KB.
-Failed to compile.
-./lib/clients/affiliate-prisma.ts
-Module not found: Can't resolve '../../node_modules/.prisma/affiliate-client'
+    at t (/vercel/path0/.next/server/webpack-runtime.js:1:128)
+    at t (/vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2290)
+    at /vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2323
+    at t.X (/vercel/path0/.next/server/webpack-runtime.js:1:1206)
+    at /vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2303
+    at Object.<anonymous> (/vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2351)
+    at Module._compile (node:internal/modules/cjs/loader:1706:14) {
+  clientVersion: '6.15.0',
+  errorCode: undefined,
+  retryable: undefined
+}
+> Build error occurred
+Error: Failed to collect page data for /api/community-challenge/register
+    at /vercel/path0/node_modules/next/dist/build/utils.js:1269:15
+    at process.processTicksAndRejections (node:internal/process/task_queues:105:5) {
+  type: 'Error'
+}
+Error: Command "npm run build" exited with 1
