7fe184055db05e86dde68a080cfb721a7572ffb4
huyennnmuk <huyennnm.uk@gmail.com>
2025-09-26 17:19:13 +0700

    feat: Fix build and automate prisma generation

--- system/Error/error.js
+++ system/Error/error.js
@@ -1,18 +1,10 @@
-Running build in Washington, D.C., USA (East) – iad1
-Build machine configuration: 2 cores, 8 GB
-Cloning github.com/huyennnmuk/yourfitnature-react (Branch: master, Commit: 2054636)
-Previous build caches not available
-Cloning completed: 22.790s
-Running "vercel build"
-Vercel CLI 48.1.6
-Installing dependencies...
-npm warn deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
-npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
-npm warn deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
-npm warn deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
-npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
-npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
-npm warn deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
-added 1083 packages in 29s
-275 packages are looking for funding
-  run `npm fund` for details
+  ▲ Next.js 14.2.32
+   Creating an optimized production build ...
+ ⚠ Found lockfile missing swc dependencies, run next locally to automatically patch
+   Disabled SWC as replacement for Babel because of custom Babel configuration "babel.config.js" https://nextjs.org/docs/messages/swc-disabled
+   Using external babel configuration from /vercel/path0/babel.config.js
+[BABEL] Note: The code generator has deoptimised the styling of /vercel/path0/node_modules/react-icons/fa/index.mjs as it exceeds the max of 500KB.
+[BABEL] Note: The code generator has deoptimised the styling of /vercel/path0/node_modules/react-icons/bi/index.mjs as it exceeds the max of 500KB.
+Failed to compile.
+./lib/clients/affiliate-prisma.ts
+Module not found: Can't resolve '../../node_modules/.prisma/affiliate-client'
