887758dddd4ab1a7ba489d5033484faa44e6f73e
huyennnmuk <huyennnm.uk@gmail.com>
2025-09-27 13:21:22 +0700

    Renormalize attributes after public override

--- system/Error/error.js
+++ system/Error/error.js
@@ -1,18 +1,24 @@
-    at t (/vercel/path0/.next/server/webpack-runtime.js:1:128)
-    at t (/vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2290)
-    at /vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2323
-    at t.X (/vercel/path0/.next/server/webpack-runtime.js:1:1206)
-    at /vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2303
-    at Object.<anonymous> (/vercel/path0/.next/server/app/api/community-challenge/register/route.js:1:2351)
-    at Module._compile (node:internal/modules/cjs/loader:1706:14) {
-  clientVersion: '6.15.0',
+19:04:49.940 
+    at oI.handleRequestError (/vercel/path0/.next/server/chunks/6936.js:114:8097)
+19:04:49.941 
+    at oI.handleAndLogRequestError (/vercel/path0/.next/server/chunks/6936.js:114:6880)
+19:04:49.941 
+    at oI.request (/vercel/path0/.next/server/chunks/6936.js:114:6587)
+19:04:49.941 
+    at async i (/vercel/path0/.next/server/chunks/6936.js:126:7515)
+19:04:49.941 
+    at async Object.s (/vercel/path0/.next/server/app/bloating-recovery-roadmap/phase-1/page.js:1:14724) {
+19:04:49.941 
+  clientVersion: '6.16.2',
+19:04:49.941 
   errorCode: undefined,
+19:04:49.941 
   retryable: undefined
+19:04:49.941 
 }
-> Build error occurred
-Error: Failed to collect page data for /api/community-challenge/register
-    at /vercel/path0/node_modules/next/dist/build/utils.js:1269:15
-    at process.processTicksAndRejections (node:internal/process/task_queues:105:5) {
-  type: 'Error'
-}
-Error: Command "npm run build" exited with 1
+19:04:49.941 
+PrismaClientInitializationError: 
+19:04:49.941 
+Invalid `prisma.product.findUnique()` invocation:
+19:04:49.941 
+19:04:49.941 
